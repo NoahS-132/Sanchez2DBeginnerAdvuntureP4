29189c65586d2745ce5d170158edc4b54f978eba
NoahS-132 <769161@student.dusd.net>
2025-12-04 11:48:17 -0800

    Movement Vector 2

--- Sanchez2DBeginnerAdvunture/Assets/Scripts/Player Contoller.cs
+++ Sanchez2DBeginnerAdvunture/Assets/Scripts/Player Contoller.cs
@@ -5,46 +5,24 @@ using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour
- {
+{
+    public InputAction MoveAction;
+
+
+
     // Start is called before the first frame update
-void Start()
-{
-
-
-}
-
-
-// Update is called once per frame
-void Update()
-{
-    float horizontal = 0.0f;
-    if (Keyboard.current.leftArrowKey.isPressed)
+    void Start()
     {
-        horizontal = -1.0f;
+        MoveAction.Enable();
     }
-    else if (Keyboard.current.rightArrowKey.isPressed)
+
+    // Update is called once per frame
+    void Update()
     {
-        horizontal = 1.0f;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+        transform.position = position;
     }
-    Debug.Log(horizontal);
-
-
-    float vertical = 0.0f;
-    if (Keyboard.current.upArrowKey.isPressed)
-    {
-        vertical = 1.0f;
-    }
-    else if (Keyboard.current.downArrowKey.isPressed)
-    {
-        vertical = -1.0f;
-    }
-    Debug.Log(vertical);
-
-
-    Vector2 position = transform.position;
-    position.x = position.x + 0.1f * horizontal;
-    position.y = position.y + 0.1f * vertical;
-    transform.position = position;
-}
 
 }
